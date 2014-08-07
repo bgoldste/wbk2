1460638342719b362d09c829226f6c548c3ba712
vagrant <vagrant@precise32.(none)>
2014-08-07 19:08:01 +0000

    urls fix

--- wbk2/urls.py
+++ wbk2/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-admin.autodiscover()
+#admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -12,6 +12,6 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
  	url(r'^', TemplateView.as_view(template_name="index.html")),
 )
