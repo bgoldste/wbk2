50293e62a072de4070845aa03f11a643f5710240
vagrant <vagrant@precise32.(none)>
2014-08-07 18:38:12 +0000

    first real commit

--- wbk2/urls.py
+++ wbk2/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
@@ -12,5 +13,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
- 
+ 	url(r'^', TemplateView.as_view(template_name="index.html")),
 )
