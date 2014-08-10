5034bb17feafe3318758c0ec879bdb0e4fdba8e5
vagrant <vagrant@precise32.(none)>
2014-08-10 18:29:23 +0000

    unicode for subscriber

--- core/models.py
+++ core/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Subscriber(models.Model):
 	email_address = models.EmailField()
+
+	def __unicode__(self):
+		return u'%s' % (self.email_address)
