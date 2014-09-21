79a2a6660f1425afe0b50ade541df35a95f7b90c
vagrant <vagrant@precise32.(none)>
2014-09-21 04:18:36 +0000

    frontend tinkering

--- core/static/js/spot.js
+++ core/static/js/spot.js
@@ -1,15 +1,36 @@
-$('#container').isotope({
-  getSortData : {
-  
-    WVHT : function ( $elem ) {
-      return $elem.find('.WVHT').text();
-    }
+$(function(){
+
+  var $container = $('#container'),
+      $checkboxes = $('#filters input');
+
+  $container.isotope({
+    itemSelector: '.item'
+   
+  });
+  // get Isotope instance
+  var isotope = $container.data('isotope');
+  // add even classes to every other visible item, in current order
+  function addEvenClasses() {
+    isotope.$filteredAtoms.each( function( i, elem ) {
+      $(elem)[ ( i % 2 ? 'addClass' : 'removeClass' ) ]('even')
+    });
   }
-});
 
+  $checkboxes.change(function(){
+    var filters = [];
+    // get checked checkboxes values
+    $checkboxes.filter(':checked').each(function(){
+      filters.push( this.value );
+    });
+    // ['.red', '.blue'] -> '.red, .blue'
+    filters = filters.join(', ');
+    $container.isotope({ filter: filters });
+    addEvenClasses();
+  });
 
+  $('#shuffle').click(function(){
+    $container.isotope('shuffle');
+    addEvenClasses();
+  });
 
-$('#container').isotope({ 
-  sortBy : 'WVHT',
-  sortAscending : false
 });
