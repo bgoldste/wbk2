15e26e05ad004ede2e20fabcf471bba1d8648f8f
vagrant <vagrant@precise32.(none)>
2014-09-21 03:36:59 +0000

    model typo chnge

--- core/management/commands/insc.py
+++ core/management/commands/insc.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from core.Models import ForecastData, Spot
+from core.models import ForecastData, Spot
 from wbk2.tasks import *
 
 class Command(BaseCommand):
